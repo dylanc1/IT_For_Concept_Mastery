0a14065dcf09189cb80cd4b60bc087857de43426
Gabriel Carver <gtcarver@crimson.ua.edu>
2024-02-29 02:27:54 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
@@ -14,9 +15,9 @@ class SessionsController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
-  
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
