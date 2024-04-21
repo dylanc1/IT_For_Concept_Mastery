e11d7e7c862d4fd5b80a3401445e696276e2d993
Holden <your.email@example.com>
2024-04-21 15:57:20 +0000

    Draggable and basic bar charts

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/custom", under: "custom"
+
+pin "@canvasjs/charts", to: "https://unpkg.com/@canvasjs/charts@3.8.2/canvasjs.min.js"
+pin "jquery", to: "https://unpkg.com/jquery@3.7.1/dist/jquery.js"
